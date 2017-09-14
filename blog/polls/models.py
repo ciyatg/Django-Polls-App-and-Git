7c5b72676d7f60951411457e8a41b96769524144
ciya <ciya>
2017-09-14 15:51:47 +0200

    pub_date text and fixing the bug

--- blog/polls/models.py
+++ blog/polls/models.py
@@ -1,13 +1,19 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 class Question(models.Model):
 
-    def __str__(self):
-        return self.question_text
-    
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
 
 class Choice(models.Model):
 
@@ -17,3 +23,4 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
