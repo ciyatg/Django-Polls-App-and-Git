3f66178af808860a599d02e8309bacb2d7d0f1c3
ciya <ciya>
2017-09-14 12:11:28 +0200

    turotial 4 was mostly on generic views.

--- ReadMe.txt
+++ ReadMe.txt
@@ -5,4 +5,6 @@ Below one can see the main topics of the tutorials with the name of the branches
  
 1. lesson1-polls: I created the first Django-powered page and started the polls app.
 2. lesson2-database: Through this tutorial I created a database for the polls app and I worked on the automatically-generated admin site. 
-3. lesson3-view: Starting from views I worked on URLpatterns and templates. 
+3. lesson3-view: Starting from views I worked on URLpatterns and templates.
+4. lesson4-template: Changed some codes with their generic version. Generic
+views help us to write shorter codes.
--- blog/polls/views.py
+++ blog/polls/views.py
@@ -3,26 +3,48 @@ from django.http import HttpResponse, HttpResponseRedirect
 #from django.template import loader
 from .models import Question, Choice
 from django.urls import reverse
+from django.views import generic
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+#def index(request):
+#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    context = {'latest_question_list': latest_question_list}
+#    return render(request, 'polls/index.html', context)
+
+#def detail(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'polls/detail.html', {'question': question})
 
 #def results(request, question_id):
     #response = "You're looking at the results of question %s."
     #return HttpResponse(response % question_id)
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+#def results(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'polls/results.html', {'question': question})
 
 #def vote(request, question_id):
     #return HttpResponse("You're voting on question %s." % question_id)
+# from tutorial4 ---->
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+# <---- from tutorial4
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
