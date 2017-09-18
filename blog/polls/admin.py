bc52e550866d95ff2210793ead23f47ee077611e
ciya <ciya>
2017-09-18 13:23:46 +0200

    Deleted the unnecessery old code.

--- blog/polls/admin.py
+++ blog/polls/admin.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-#class QuestionAdmin(admin.ModelAdmin):
-#    fields = ['pub_date', 'question_text']
-
-#admin.site.register(Question, QuestionAdmin)
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
--- blog/polls/views.py
+++ blog/polls/views.py
@@ -7,25 +7,6 @@ from django.views import generic
 from django.utils import timezone
 
 
-#def index(request):
-#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#    context = {'latest_question_list': latest_question_list}
-#    return render(request, 'polls/index.html', context)
-
-#def detail(request, question_id):
-#    question = get_object_or_404(Question, pk=question_id)
-#    return render(request, 'polls/detail.html', {'question': question})
-
-#def results(request, question_id):
-    #response = "You're looking at the results of question %s."
-    #return HttpResponse(response % question_id)
-
-#def results(request, question_id):
-#    question = get_object_or_404(Question, pk=question_id)
-#    return render(request, 'polls/results.html', {'question': question})
-
-#def vote(request, question_id):
-    #return HttpResponse("You're voting on question %s." % question_id)
 # from tutorial4 ---->
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
